d7f201a9624a5c143429c11ac040f27093db9713
Danila <silnovdanila2@gmail.com>
2026-07-05 13:32:19 +0300

    Initial commit

---
+++ src/main/java/com/artix/bonus/BonusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.artix.bonus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BonusBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artix/bonus/BonusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artix.bonus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BonusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
